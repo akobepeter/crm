0c489bf411673d2bec2a3983d6511618d1f2518f
akobepeter <mrpeterakobe@gmail.com>
2023-10-20 13:35:06 +0100

    push

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -66,15 +66,6 @@
                         </a>
                     </li>
                     <li class=" sidebar-layout">
-                        <a href="../backend/pages-maintenance.html" class="svg-icon">
-                            <i class="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
-                                </svg>
-                            </i><span class="ml-2">Maintenance</span>
-                        </a>
-                    </li>
-                    <li class=" sidebar-layout">
                         <a href="{{route('task')}}" class="svg-icon">
                             <i class="">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" fill="none" viewBox="0 0 24 24" stroke="currentColor">
